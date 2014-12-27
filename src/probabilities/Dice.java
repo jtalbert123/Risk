45fefe8d7c5bc8f49c130d201086fae7fdb837a3
James Talbert <jtalbert123@gmail.com>
2014-12-27 17:36:13 -0600

    The program can compute the probabilities of any given dice combination (eg: attacker rols 3 and defender rolls 2, the attacker will probably win any given roll of the dice, so if both have a similar number of armies, the attaker will probably win the battle overall).

--- src/probabilities/Dice.java
+++ src/probabilities/Dice.java
@@ -4,20 +4,139 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
-import sun.org.mozilla.javascript.internal.ObjArray;
-
 public class Dice {
 
+	public static void main(String[] args) {
+		Dice d = new Dice();
+		System.out.println(d.getProbability(3, 1));
+		System.out.println(d.getProbability(1, 2));
+	}
+
 	private Object[][][][] outcomes = new Object[3][2][][];
-	
+
 	public Dice() {
-		for (int aDice = 0; aDice < 3; aDice++) {
-			for (int dDice = 0; dDice < 2; dDice++) {
-				outcomes[aDice][dDice] = makeSet(aDice, dDice);
+		for (int aDice = 1; aDice <= 3; aDice++) {
+			for (int dDice = 1; dDice <= 2; dDice++) {
+				outcomes[aDice - 1][dDice - 1] = makeSet(aDice, dDice);
+
+				Object[][] set = outcomes[aDice - 1][dDice - 1];
+
+				if (aDice == 1) {
+					if (dDice == 1) {
+						for (int a1 = 0; a1 < 6; a1++) {
+							for (int d1 = 0; d1 < 6; d1++) {
+								set[a1][d1] = new Battle(new Integer[] { a1 },
+										new Integer[] { d1 });
+							}
+						}
+					} else if (dDice == 2) {
+						for (int a1 = 0; a1 < 6; a1++) {
+							for (int d1 = 0; d1 < 6; d1++) {
+								for (int d2 = 0; d2 < 6; d2++) {
+									((Battle[][][]) set)[a1][d1][d2] = new Battle(
+											new Integer[] { a1 },
+											new Integer[] { d1, d2 });
+								}
+							}
+						}
+					}
+				} else if (aDice == 2) {
+					if (dDice == 1) {
+						for (int a1 = 0; a1 < 6; a1++) {
+							for (int a2 = 0; a2 < 6; a2++) {
+								for (int d1 = 0; d1 < 6; d1++) {
+									((Battle[][][]) set)[a1][a2][d1] = new Battle(
+											new Integer[] { a1, a2 },
+											new Integer[] { d1 });
+								}
+							}
+						}
+					} else if (dDice == 2) {
+						for (int a1 = 0; a1 < 6; a1++) {
+							for (int a2 = 0; a2 < 6; a2++) {
+								for (int d1 = 0; d1 < 6; d1++) {
+									for (int d2 = 0; d2 < 6; d2++) {
+										((Battle[][][][]) set)[a1][a2][d1][d2] = new Battle(
+												new Integer[] { a1, a2 },
+												new Integer[] { d1, d2 });
+									}
+								}
+							}
+						}
+					}
+				} else if (aDice == 3) {
+					if (dDice == 1) {
+						for (int a1 = 0; a1 < 6; a1++) {
+							for (int a2 = 0; a2 < 6; a2++) {
+								for (int a3 = 0; a3 < 6; a3++) {
+									for (int d1 = 0; d1 < 6; d1++) {
+										((Battle[][][][]) set)[a1][a2][a3][d1] = new Battle(
+												new Integer[] { a1, a2, a3 },
+												new Integer[] { d1 });
+									}
+								}
+							}
+						}
+					} else if (dDice == 2) {
+						for (int a1 = 0; a1 < 6; a1++) {
+							for (int a2 = 0; a2 < 6; a2++) {
+								for (int a3 = 0; a3 < 6; a3++) {
+									for (int d1 = 0; d1 < 6; d1++) {
+										for (int d2 = 0; d2 < 6; d2++) {
+											((Battle[][][][][]) set)[a1][a2][a3][d1][d2] = new Battle(
+													new Integer[] { a1, a2, a3 },
+													new Integer[] { d1, d2 });
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
-	
+
+	public double getProbability(int aDice, int dDice) {
+		Object[][] set = outcomes[aDice - 1][dDice - 1];
+		int wins = 0;
+		int totalOutcomes = (int) Math.pow(6, aDice + dDice);
+		for (Object[] o : set) {
+			for (Object p : o) {
+				if (Object[].class.isInstance(p)) {
+					for (Object q : (Object[]) p) {
+						if (Object[].class.isInstance(q)) {
+							for (Object r : (Object[]) q) {
+								if (Battle[].class.isInstance(r)) {
+									for (Battle s : (Battle[]) r) {
+										int aWins = s.getDefenderLosses();
+										int dWins = s.getAttackerLosses();
+										wins += aWins - dWins;
+									}
+								} else {
+									int aWins = ((Battle) r)
+											.getDefenderLosses();
+									int dWins = ((Battle) r)
+											.getAttackerLosses();
+									wins += aWins - dWins;
+								}
+							}
+						} else {
+							int aWins = ((Battle) q).getDefenderLosses();
+							int dWins = ((Battle) q).getAttackerLosses();
+							wins += aWins - dWins;
+						}
+					}
+				} else {
+					int aWins = ((Battle) p).getDefenderLosses();
+					int dWins = ((Battle) p).getAttackerLosses();
+					wins += aWins - dWins;
+				}
+			}
+		}
+		return (((double)wins)/((double)totalOutcomes))*100.0;
+	}
+
 	public static Object[][] makeSet(int attackerDice, int defenderDice) {
 		int length = attackerDice + defenderDice;
 		Object[][] battles;
@@ -34,28 +153,28 @@ public class Dice {
 		}
 		return battles;
 	}
-	
+
 	private class Battle {
 
 		Integer[] attacker;
 		Integer[] defender;
-		
+
 		public Battle(Integer[] attacker, Integer[] defender) {
 			this.attacker = attacker;
 			this.defender = defender;
-			
+
 			Comparator<Integer> descending = new Comparator<Integer>() {
-				
+
 				@Override
 				public int compare(Integer o1, Integer o2) {
 					return o2 - o1;
 				}
 			};
-			
+
 			Collections.sort(Arrays.asList(this.attacker), descending);
 			Collections.sort(Arrays.asList(this.defender), descending);
 		}
-		
+
 		public int getAttackerLosses() {
 			int losses = 0;
 			for (int i = 0; i < attacker.length && i < defender.length; i++) {
@@ -65,7 +184,7 @@ public class Dice {
 			}
 			return losses;
 		}
-		
+
 		public int getDefenderLosses() {
 			int losses = 0;
 			for (int i = 0; i < attacker.length && i < defender.length; i++) {
